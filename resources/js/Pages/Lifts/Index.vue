c2a6e399d77e9e0f0a8ae8730cd80dc50d9a55ba
Igors Koptevs <igors.koptevs@gmail.com>
2022-06-05 23:58:35 +0300

    Createf Lifts Index page

---
+++ resources/js/Pages/Lifts/Index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="p-4">
+        <h1 class="text-4xl">Hellou!</h1>
+        <section v-for="lift of lifts">
+            <h1>{{ lift.reg_number }}</h1>
+            <pre>{{ lift }}</pre>
+        </section>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "index",
+    props: {
+        lifts: Array,
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
