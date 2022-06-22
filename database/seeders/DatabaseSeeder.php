2a813304d5fc05e6f7fbb8e0851ceaa0c9557bc8
Igors Koptevs <igors.koptevs@gmail.com>
2022-06-22 13:02:58 +0300

    Added Lift Managers seeder to database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,8 @@ class DatabaseSeeder extends Seeder
         User::factory(10)->create();
         $this->call([
             LiftSeeder::class,
-            AdminSeeder::class
+            AdminSeeder::class,
+            LiftManagerSeeder::class,
         ]);
         // \App\Models\User::factory(10)->create();
 
