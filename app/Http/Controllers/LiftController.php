c82502ab71dc1e4b46c0fa2b46452753e2f2867e
Igors Koptevs <igors.koptevs@gmail.com>
2022-06-22 13:34:02 +0300

    Added return type to LiftController

--- app/Http/Controllers/LiftController.php
+++ app/Http/Controllers/LiftController.php
@@ -5,10 +5,16 @@ namespace App\Http\Controllers;
 use App\Models\Lift;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use Inertia\Response;
 
 class LiftController extends Controller
 {
-    public function index()
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index(): Response
     {
         $lifts = Lift::all();
         return Inertia::render('Lifts/Index', ['lifts' => $lifts]);
